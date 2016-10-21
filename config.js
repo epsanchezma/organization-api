5ebea254409f34116c05e0528fadc67006f77a23
Elba Sanchez Marquez <bita.sanchez2991@gmail.com>
2016-10-21 17:49:49 -0500

    Change Mongo URI

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const key = 'secretkey';
 const dbUrl = {
   test: 'mongodb://localhost:27017/organization-api-test',
   development: 'mongodb://localhost:27017/organization-api',
-  production: process.env.MONGOHQ_URL
+  production: process.env.MONGODB_URI
 }
 
 module.exports = {
